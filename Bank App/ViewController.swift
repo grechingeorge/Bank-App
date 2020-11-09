9df0269a30ef10b2647824d475bc3d9fcc793c52
George <grechin58@mail.ru>
2020-11-09 12:46:33 +0300

    Initial Commit

---
+++ Bank App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bank App
+//
+//  Created by Георгий Гречин on 09.11.2020.
+//  Copyright © 2020 George Grechin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
